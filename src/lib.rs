e2344980f8ac66fa717bf90e70474b298805394b
Dan Castrillo <126793278+dvrd@users.noreply.github.com>
2023-07-24 22:22:18 -0400

    feat: draw lines & circles

--- src/lib.rs
+++ src/lib.rs
@@ -30,6 +30,81 @@ impl Canvas {
         }
     }
 
+    pub fn add_rect(&mut self, x: i32, y: i32, w: i32, h: i32, color: u32) {
+        for dy in 0..h {
+            let y = y + dy;
+            if y > 0 && self.height as i32 > y {
+                for dx in 0..w {
+                    let x = x + dx;
+                    if x > 0 && self.width as i32 > x {
+                        self.paint_pixel(x, y, color);
+                    }
+                }
+            }
+        }
+    }
+
+    pub fn add_circle(&mut self, cx: i32, cy: i32, r: usize, color: u32) {
+        let x1 = cx - r as i32;
+        let y1 = cy - r as i32;
+        let x2 = cx + r as i32;
+        let y2 = cy + r as i32;
+
+        for y in y1..y2 {
+            if y > 0 && self.height as i32 > y {
+                for x in x1..x2 {
+                    if x > 0 && self.width as i32 > x {
+                        let dx = x - cx;
+                        let dy = y - cy;
+                        if dx.pow(2) + dy.pow(2) <= r.pow(2) as i32 {
+                            self.paint_pixel(x, y, color);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    fn get_position(&self, x: i32, y: i32) -> usize {
+        (y * self.width as i32 + x) as usize
+    }
+
+    fn paint_pixel(&mut self, x: i32, y: i32, color: u32) {
+        let position = self.get_position(x, y);
+        self.pixels[position] = color;
+    }
+
+    pub fn draw_line(&mut self, x1: i32, y1: i32, x2: i32, y2: i32, color: u32) {
+        let dx = x2 - x1;
+        let dy = y2 - y1;
+        if dx != 0 {
+            let c = y1 - dy * x1 / dx;
+            let (x1, x2) = if x1 > x2 { (x2, x1) } else { (x1, x2) };
+            for x in x1..=x2 {
+                if x >= 0 && self.width as i32 > x {
+                    let y = dy * x / dx + c;
+                    let ny = dy * (x + 1) / dx + c;
+                    let (y, ny) = if y > ny { (ny, y) } else { (y, ny) };
+                    for y in y..=ny {
+                        if y >= 0 && self.height as i32 > y {
+                            self.paint_pixel(x, y, color);
+                        }
+                    }
+                }
+            }
+        } else {
+            let x = x1;
+            if x >= 0 && self.width as i32 > x {
+                let (y1, y2) = if y1 > y2 { (y2, y1) } else { (y1, y2) };
+                for y in y1..y2 {
+                    if y >= 0 && self.height as i32 > y {
+                        self.paint_pixel(x, y, color);
+                    }
+                }
+            }
+        }
+    }
+
     pub fn save_to_ppm(&self, path: &str) -> Result<(), ()> {
         let mut file = File::create(path).map_err(|msg| {
             eprintln!("Error: file {path} could not be open/created: {msg}");
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,117 @@
-mod lib;
-
 use lapiz::Canvas;
 
 const WIDTH: usize = 800;
 const HEIGHT: usize = 600;
+const COLS: usize = 8 * 2;
+const ROWS: usize = 6 * 2;
+const CELL_WIDTH: usize = WIDTH / COLS;
+const CELL_HEIGTH: usize = HEIGHT / ROWS;
 const SIZE: usize = WIDTH * HEIGHT;
+
 static mut PIXELS: [u32; SIZE] = [0; SIZE];
 
+const RED: u32 = 0xff2020ff;
+const GREEN: u32 = 0xff00ff00;
+const BLUE: u32 = 0xffff0000;
+const GREY: u32 = 0xff202020;
+// const WHITE: u32 = 0xffffffff;
+
 fn main() -> Result<(), ()> {
-    let file_path = "o.ppm";
+    checkers_example()?;
+    Ok(())
+}
+
+fn lerp(a: f32, b: f32, t: f32) -> f32 {
+    a + (b - a) * t
+}
+
+/// Examples
+#[allow(dead_code)]
+fn line_example() -> Result<(), ()> {
+    let file_path = "lines.ppm";
 
     unsafe {
         let mut canvas = Canvas::new(&mut PIXELS, WIDTH, HEIGHT);
-        canvas.fill(0xff00ff00);
+        canvas.fill(GREY);
+        canvas.draw_line(0, 0, WIDTH as i32, HEIGHT as i32, RED);
+        canvas.draw_line(WIDTH as i32, 0, 0, HEIGHT as i32, RED);
+
+        canvas.draw_line(0, 0, WIDTH as i32 / 4, HEIGHT as i32, GREEN);
+        canvas.draw_line(WIDTH as i32 / 4, 0, 0, HEIGHT as i32, GREEN);
+
+        canvas.draw_line(WIDTH as i32, 0, WIDTH as i32 / 4 * 3, HEIGHT as i32, GREEN);
+        canvas.draw_line(WIDTH as i32 / 4 * 3, 0, WIDTH as i32, HEIGHT as i32, GREEN);
+
+        canvas.draw_line(0, HEIGHT as i32 / 2, WIDTH as i32, HEIGHT as i32 / 2, BLUE);
+        canvas.draw_line(WIDTH as i32 / 2, 0, WIDTH as i32 / 2, HEIGHT as i32, BLUE);
+        // for y in 0..(ROWS * 2) {
+        //     for x in 0..(COLS * 2) {
+        //         let u = x as f32 / COLS as f32;
+        //         let v = y as f32 / ROWS as f32;
+        //         let t = (u + v) / 2.;
+
+        //         let circle_r = if CELL_HEIGTH < CELL_WIDTH {
+        //             CELL_HEIGTH
+        //         } else {
+        //             CELL_WIDTH
+        //         };
+        //         let x = x * CELL_WIDTH + CELL_WIDTH / 2;
+        //         let y = y * CELL_HEIGTH + CELL_HEIGTH / 2;
+        //         let circle_r = lerp(circle_r as f32 / 4., circle_r as f32, t) as usize;
+        //         canvas.add_circle(x as i32, y as i32, circle_r / 2, RED);
+        //     }
+        // }
+        canvas.save_to_ppm(file_path)?;
+    };
+    Ok(())
+}
+
+#[allow(dead_code)]
+fn circle_example() -> Result<(), ()> {
+    let file_path = "circle.ppm";
+
+    unsafe {
+        let mut canvas = Canvas::new(&mut PIXELS, WIDTH, HEIGHT);
+        canvas.fill(GREY);
+        for y in 0..(ROWS) {
+            for x in 0..(COLS) {
+                let u = x as f32 / COLS as f32;
+                let v = y as f32 / ROWS as f32;
+                let t = (u + v) / 2.;
+
+                let circle_r = if CELL_HEIGTH < CELL_WIDTH {
+                    CELL_HEIGTH
+                } else {
+                    CELL_WIDTH
+                };
+                let x = x * CELL_WIDTH + CELL_WIDTH / 2;
+                let y = y * CELL_HEIGTH + CELL_HEIGTH / 2;
+                let circle_r = lerp(circle_r as f32 / 4., circle_r as f32, t) as usize;
+                canvas.add_circle(x as i32, y as i32, circle_r / 2, RED);
+            }
+        }
+        canvas.save_to_ppm(file_path)?;
+    };
+    Ok(())
+}
+
+#[allow(dead_code)]
+fn checkers_example() -> Result<(), ()> {
+    let file_path = "checkers.ppm";
+    let rect_w = 50 * 4;
+    let rect_h = 30 * 4;
+
+    unsafe {
+        let mut canvas = Canvas::new(&mut PIXELS, WIDTH, HEIGHT);
+        canvas.fill(GREY);
+        for y in 0..ROWS {
+            for x in 0..COLS {
+                let color = if (x + y) % 2 == 0 { RED } else { GREY };
+                let x = x * CELL_WIDTH;
+                let y = y * CELL_HEIGTH;
+                canvas.add_rect(x as i32, y as i32, rect_w, rect_h, color);
+            }
+        }
         canvas.save_to_ppm(file_path)?;
     };
     Ok(())
